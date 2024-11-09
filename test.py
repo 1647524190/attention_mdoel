939346e84418b2132201d85c942f3d99e6f6a3a1
tusong <1647524190@qq.com>
2024-11-10 00:36:13 +0800

    test: 添加多尺度unfold策略，无法生效

--- test.py
+++ test.py
@@ -64,34 +64,33 @@ class DPConv(nn.Module):
             extension: 扩充大小，获取更大范围的特征信息
         """
         super(DPConv, self).__init__()
+        if not (kernel > 2 and kernel % 2 == 0):
+            raise ValueError("Kernel size must be an even number greater than 2.")
         self.in_channels = in_channels
         self.bottleneck_channels = (self.in_channels // 4) * 4
         self.out_channels = out_channels
         self.kernel = kernel
         self.kernel_list = [self.kernel // 2, self.kernel, self.kernel + self.kernel // 2]
         self.stride = stride
+        self.padding = self.kernel // 2
 
-        self.reduce = nn.Conv2d(self.in_channels, self.in_channels // 4, kernel_size=1, stride=1)
+        # self.reduce = nn.Conv2d(self.in_channels, self.in_channels // 4, kernel_size=1, stride=1)
+        self.conv = nn.Conv2d(self.in_channels, self.in_channels, kernel_size=1, stride=1)
         self.pe = nn.Conv2d(self.in_channels, self.in_channels, kernel_size=3, stride=1, padding=1,
                             groups=self.in_channels)
-        # self.attention = Attention(self.in_channels, num_heads=self.out_channels // 64)
+        self.attention = Attention(self.in_channels, num_heads=self.out_channels // 64)
+        self.weight = nn.Parameter(torch.ones(len(self.kernel_list) + 1))
 
     def forward(self, x):
+        print("输入特征均值mean:", x.mean().item(), "输入特征方差std:", x.std().item())
         N, C, H, W = x.shape
-        # 尺寸修剪，确保均为偶数
-        if H % 2 != 0:
-            x = x[:, :, :-1, :]
-        if W % 2 != 0:
-            x = x[:, :, :, :-1]
-        # print("修剪后的x的形状：" + str(x.shape))
-        x = F.pad(x, (1, 1, 1, 1), mode='replicate')
-        x = self.reduce(x)
-        N, C, H, W = x.shape
-        print("填充后的x的形状：" + str(x.shape))
+        # x = F.pad(x, (self.padding, self.padding, self.padding, self.padding), mode='replicate')
+        # N, C, H_pad, W_pad = x.shape
 
         # 多尺度unfold核展开
-        x_unfolded = []
+        out_list = [x]
         for i in range(len(self.kernel_list)):
+            print("##############################################################################")
             print("第" + str(i) + "次展开：")
             # 对张量进行unfold展开，分解为 num_windows * num_windows 个子块
             # (N, C * self.kernel_list[i] * self.kernel_list[i], num_windows)
@@ -102,27 +101,49 @@ class DPConv(nn.Module):
             unfolded = unfolded.view(N * num_windows, C, self.kernel_list[i], self.kernel_list[i])
             print("view之后的unfolded的形状: " + str(unfolded.shape))
 
-            if self.kernel_list[i] < self.kernel:
-                unfolded = F.interpolate(unfolded, size=(self.kernel, self.kernel), mode='bilinear').view(N, num_windows, C, self.kernel, self.kernel)
-                print("上采样之后的unfold的形状：" + str(unfolded.shape))
-                x_unfolded.append(unfolded)
-            elif self.kernel_list[i] > self.kernel:
-                unfolded = F.adaptive_avg_pool2d(unfolded, output_size=(self.kernel, self.kernel)).view(N, num_windows, C, self.kernel, self.kernel)
-                x_unfolded.append(unfolded)
-                print("池化之后的unfold的形状：" + str(unfolded.shape))
-            else:
-                x_unfolded.append(unfolded.view(N, num_windows, C, self.kernel, self.kernel))
-                print("unfold的形状：" + str(unfolded.view(N, num_windows, C, self.kernel, self.kernel).shape))
-        x_unfolded_stack = torch.cat(x_unfolded, dim=1)
-        print("堆叠后的x_unfolded_stack的形状：" + str(x_unfolded_stack.shape))
-        pass
+            # 输入注意力模块
+            print("输入注意力模块前均值mean:", unfolded.mean().item(), "特征方差std:", unfolded.std().item())
+            # attention_output = self.attention(self.pe(unfolded) + unfolded)
+            attention_output = unfolded
+            print("输入注意力模块后均值mean:", attention_output.mean().item(), "特征方差std:", attention_output.std().item())
+
+            # attention_output = pooled_regions
+            # print("attention的形状： " + str(attention_output.shape))
+
+            # 转化为(N, C * self.kernel_list[i] * self.kernel_list[i], num_windows)，便于进行fold操作
+            attention_output = attention_output.view(N, C * self.kernel_list[i] * self.kernel_list[i], num_windows)
+            # print("view之后的attention的形状： " + str(attention_output.shape))
+
+            # 计算重叠部分的权重
+            count = F.fold(torch.ones_like(attention_output), output_size=(H, W), kernel_size=self.kernel_list[i], stride=self.stride)
+            # print("count 形状: " + str(count.shape))
+
+            # 重新通过 fold 将滑动窗口展开为完整的张量，形状为(N, C, H_orin, W_orin)
+            attention_output = F.fold(attention_output, output_size=(H, W), kernel_size=self.kernel_list[i], stride=self.stride)
+            # print("fold后的形状: " + str(output.shape))
+
+            # 对重叠部分取平均值
+            attention_output = attention_output / count
+            print(f"第 {i} 输出均值mean:", attention_output.mean().item(), "std:", attention_output.std().item())
+            print("第" + str(i) + "个输出形状：" + str(attention_output.shape))
+            out_list.append(attention_output)
+
+        out = torch.stack(out_list).sum(dim=0)
+        # print(sum_tensor.shape)
+        out = out / len(out_list)
+        print("####################################################################################################################")
+        print("输入特征均值mean:", x.mean().item(), "输入特征方差std:", x.std().item())
+        print("输出特征均值mean:", out.mean().item(), "输入特征方差std:", out.std().item())
+        # print("out 形状: " + str(out.shape))
+
+        return out
 
 
 # 测试代码
 if __name__ == '__main__':
     # x = torch.randn(2, 2, 4, 4)
     # x = torch.randn(4, 256, 762, 524)
-    x = torch.randn(1, 576, 9, 8)
+    x = torch.randn(4, 576, 20, 20)
 
     attention = DPConv(x.shape[1], x.shape[1], 4, 2)
     flops, params = profile(attention, inputs=(x,))
